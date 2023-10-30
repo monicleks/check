d8911417aac20cc8b18b097a9aa4f920d90e88db
Monicleks <romanmatias68@gmail.com>
2023-10-30 17:02:06 -0300

    Agregué estilos CSS

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  login(rut: string, password: string) { // terminar en otra ocasion 
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
